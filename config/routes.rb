dfa7fd467606bb42660a9705f0a60446e2939141
Patrick Mallee <patmallee@gmail.com>
2016-01-14 16:06:06 +0000

    Create Sign in form

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,16 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+    root 'sessions#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+    get 'login' => 'sessions#new'
+    post 'login' => 'sessions#create'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
